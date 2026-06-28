1e4df8da8accaef1c57fbf30308836202e37c951
pommicket <pommicket@gmail.com>
2026-06-27 20:47:23 -0400

    new list format

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-import { readRequest, formatResponseError, STICKERS_PER_COUNTRY, COUNTRY_IDS, getSortInversePermutation, stickerCode } from "./common.js?v2";
+import { readRequest, formatResponseError, STICKERS_PER_COUNTRY, COUNTRY_IDS, getSortInversePermutation, stickerName } from "./common.js?v2";
 // show "just missing" / "just duplicates"
 const publicID = searchParams.get('missing') || searchParams.get('duplicates');
 if (!publicID) location.href = '404.html';
@@ -15,19 +15,27 @@ readRequest(publicID).then(async response => {
 		listContainer.replaceChildren();
 		for (let c = 0; c < COUNTRY_IDS.length; c++) {
 			let country = getSortInversePermutation(sortBy, c);
+			let teamContainer = document.createElement('div');
+			teamContainer.append(COUNTRY_IDS[country], ': ');
+			let first = true;
 			for (let s = 0; s < STICKERS_PER_COUNTRY; s++) {
 				const i = country * STICKERS_PER_COUNTRY + s;
 				const n = data[i];
 				if (!(checkMissing ? n === 0 : n > 1)) continue;
-				let sticker = document.createElement('div');
-				sticker.textContent = stickerCode(i);
+				if (!first) {
+					teamContainer.append(', ');
+				}
+				first = false;
+				teamContainer.append(stickerName(country, s));
 				if (n > 2) {
 					let dupCount = document.createElement('span');
 					dupCount.classList.add('dup-count');
 					dupCount.textContent = '×' + (n-1);
-					sticker.append(dupCount);
+					teamContainer.append(dupCount);
 				}
-				listContainer.append(sticker);
+			}
+			if (!first) {
+				listContainer.append(teamContainer);
 			}
 		}
 	}
